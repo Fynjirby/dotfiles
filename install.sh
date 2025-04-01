5f0e5c7fe6f71272021f9118d32a9a81044a651b
Egor <109822937+Fynjirby@users.noreply.github.com>
2025-04-01 21:18:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,35 @@
-echo ".vimrc, .zshrc, .tmux.conf will be cloned from github to your pc and loaded with source command"
+echo "LOG: .vimrc, .zshrc, .tmux.conf will be cloned from github to your pc and moved to ~ "
 git clone https://github.com/Fynjirby/dotfiles.git dotfiles
-echo "cloned successfully"
+echo "LOG: cloned successfully"
 
-echo "moving configs (may require user password)"
+echo "LOG: moving configs (may require user password)"
 sudo mv dotfiles/.vimrc ~/.vimrc
 sudo mv dotfiles/.zshrc ~/.zshrc
 sudo mv dotfiles/.tmux.conf ~/.tmux.conf
 sudo mkdir ~/.config
 sudo mv dotfiles/.config/starship.toml ~/.config/starship.toml 
-echo "moved successfully"
+echo "LOG: moved successfully"
 
-echo "installing oh my zsh (if fails please check are zsh and curl installed)"
+echo "LOG: installing oh my zsh (if fails please check are zsh and curl installed)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "oh my zsh installed successfully"
+echo "LOG: oh my zsh installed successfully"
 
 echo "installing zsh plugins"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "zsh plugins installed successfully"
+echo "LOG: zsh plugins installed successfully"
 
-echo "installing vim plug"
+echo "LOG: installing starship"
+curl -sS https://starship.rs/install.sh | sh
+echo "LOG: staship installed successfully"
+
+echo "LOG: installing vim plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "vim plug installed successfully"
+echo "LOG: vim plug installed successfully"
 
-echo "removing temp folder"
+echo "LOG: removing temp folder"
 rm -rf ./dotfiles/
 
-echo "all installed successfully, thanks for using"
+echo "LOG: all installed successfully, thanks for using"
+echo "LOG: next use this command to load .zshrc \n source ~/.zshrc"
