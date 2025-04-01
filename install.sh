9c34f433ac09035197e4986fa50c8a294b229eae
Egor <109822937+Fynjirby@users.noreply.github.com>
2025-04-01 13:19:12 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,13 @@ sudo mv dotfiles/.zshrc ~/.zshrc
 sudo mv dotfiles/.tmux.conf ~/.tmux.conf
 echo "moved successfully"
 
+echo "installing oh my zsh (if fails please check are zsh and curl installed"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "oh my zsh installed successfully"
+
+echo "installing vim plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "vim plug installed successfully"
+
 rm -rf ./dotfiles/
