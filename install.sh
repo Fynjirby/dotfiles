fa3e3e772eeb81d9f29665e0437b1ed17d25c71d
Egor <109822937+Fynjirby@users.noreply.github.com>
2025-08-25 16:25:00 +0300

    back to zsh after month with fish :)

--- install.sh
+++ install.sh
@@ -5,27 +5,36 @@ echo "LOG: cloned successfully"
 echo "LOG: moving old configs to ~/.old"
 mkdir ~/.old
 mv ~/.vimrc ~/.old/.vimrc
+mv ~/.zshrc ~/.old/.zshrc
 mv ~/.tmux.conf ~/.old/.tmux.conf
 mkdir ~/.old/.config
-mv ~/.config/fish/config.fish ~/.old/.config/fish/config.fish
 mv ~/.config/alacritty.toml ~/.old/.config/alacritty.toml
 mv ~/.config/starship.toml ~/.old/.config/starship.toml 
 echo "LOG: moved successfully"
 
 rm -rf ~/.vimrc
+rm -rf ~/.zshrc
 rm -rf ~/.tmux.conf
-rm -rf ~/.config/fish/config.fish
 rm -rf ~/.config/alacritty.toml
 rm -rf ~/.config/starship.toml
 
 echo "LOG: moving new configs (may require user password)"
 mv dotfiles/.vimrc ~/.vimrc
+mv dotfiles/.zshrc ~/.zshrc
 mv dotfiles/.tmux.conf ~/.tmux.conf
-mkdir -p ~/.config/fish
-mv dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+mkdir ~/.config
 mv dotfiles/.config/alacritty.toml ~/.config/alacritty.toml
 mv dotfiles/.config/starship.toml ~/.config/starship.toml 
 echo "LOG: moved successfully"
+
+echo "LOG: installing oh my zsh (if fails please check are zsh and curl installed)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "LOG: oh my zsh installed successfully"
+
+echo "installing zsh plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "LOG: zsh plugins installed successfully"
 
 echo "LOG: installing starship"
 curl -sS https://starship.rs/install.sh | sh
@@ -43,5 +52,6 @@ echo "LOG: removing temp folder"
 rm -rf ./dotfiles/
 
 echo "LOG: all installed successfully, thanks for using"
-echo "LOG: install fish from fishshell.com (guides for various PMs are there)"
-echo "LOG: also install alacritty from alacritty.org or via your package manager"
+echo "LOG: next use this command to load .zshrc"
+echo "LOG: source ~/.zshrc"
+echo "LOG: Also install alacritty from alacritty.org or via your package manager"
