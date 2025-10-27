4a9a41ae9dfc746d608a0b70d914ec411685f3a8
Egor <fynjirby@icloud.com>
2025-10-27 10:16:24 +0300

    update hypr config

---
+++ .config/hypr/suspend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+zenity --question \
+  --title="" \
+  --window-icon=system-shutdown \
+  --text="<b>Bro do you really wanna suspend $(uname -n)?</b>" \
+  --ok-label="Yes" \
+  --cancel-label="No" \
+  --default-cancel \
+  --no-wrap 
+if [ $? -eq 0 ]; then
+    systemctl suspend
+fi
